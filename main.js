3c2785b80af2412f39e52be455510581ae659132
David Castagneto A <davidcastagnetoa@gmail.com>
2023-04-06 17:26:25 +0200

    hello world

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log('hello there');
+const { app, BrowserWindow } = require("electron");
+
+const createWindow = () => {
+  const window = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+
+  window.loadFile("index.html");
+};
+
+app.whenReady().then(()=>{
+    createWindow()
+})
