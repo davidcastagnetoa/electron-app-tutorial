581d36f342f5b60ba4ee0a4d619ec45359f7527c
David Castagneto A <davidcastagnetoa@gmail.com>
2023-04-07 16:03:19 +0200

    electron app README

--- main.js
+++ main.js
@@ -4,11 +4,14 @@ const createWindow = () => {
   const window = new BrowserWindow({
     width: 800,
     height: 600,
+    minHeight: 400,
+    minWidth: 400
   });
 
-  window.loadFile("index.html");
+  // window.loadFile("index.html"); //load a HTML file
+  window.loadURL("https://moonshotguide.github.io/"); //load a url
 };
 
 app.whenReady().then(()=>{
-    createWindow()
+    createWindow() // window
 })
