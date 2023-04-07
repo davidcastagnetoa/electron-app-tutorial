8b03828475d7a13d0d7910a53321ea15e0032b08
David Castagneto A <davidcastagnetoa@gmail.com>
2023-04-07 17:19:48 +0200

    Custom menu, and Show and Hide window on top Toggle button

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# electron-app-tutorial
+# Electron Application tutorial
+
+### Instructions
+
+[Link to instructions](https://www.electronjs.org/docs/latest/tutorial/tutorial-first-app)
+
+- Create a folder ```mkdir electron-app```
+- Run ```npm init -y```
+- Install dependency ```npm install electron --save-dev```
+- Add in package.json in the scripts line ```"dev": "electron ."``` and edit main line ```"main": "main.js"```
+- Create ```main.js``` in main.js add this code:
+   
+``` const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
+
+```
--- main.js
+++ main.js
@@ -1,17 +1,75 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, ipcMain, Menu } = require("electron");
+const path = require("path");
 
-const createWindow = () => {
-  const window = new BrowserWindow({
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     minHeight: 400,
-    minWidth: 400
+    minWidth: 400,
+    webPreferences: {
+      nodeIntegration: true, // Enable Node.js integration
+      contextIsolation: false, // Disable context isolation to allow access to Electron APIs from the renderer process
+    },
   });
 
-  // window.loadFile("index.html"); //load a HTML file
-  window.loadURL("https://moonshotguide.github.io/"); //load a url
-};
+  mainWindow.loadFile(path.join(__dirname, "index.html"));
+  // o mainWindow.loadURL("https://moonshotguide.github.io/");
 
-app.whenReady().then(()=>{
-    createWindow() // window
-})
+  // Create a personal menú
+  const template = [
+    {
+      label: "Archivo",
+      submenu: [
+        {
+          label: "Abrir",
+          accelerator: "CmdOrCtrl+O", // Atajo de teclado para abrir el menú
+          click() {
+            console.log("Abriendo archivo...");
+          },
+        },
+        {
+          label: "Cerrar",
+          accelerator: "CmdOrCtrl+1", // Atajo de teclado para abrir el menú
+          click() {
+            console.log("Cerrando archivo...");
+          },
+        },
+      ],
+    },
+  ];
+
+  const menu = Menu.buildFromTemplate(template);
+  
+
+  // Oculta el menú por defecto
+  // Menu.setApplicationMenu(null);
+  // Muestra el menu
+  Menu.setApplicationMenu(menu);
+
+  mainWindow.on("closed", function () {
+    mainWindow = null;
+  });
+}
+
+//Load the window
+app.whenReady().then(() => {
+  createWindow();
+});
+
+app.on("activate", function () {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
+
+//Close Window
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") app.quit();
+});
+
+// IPC handler for toggling the always on top setting
+ipcMain.on("toggle-always-on-top", (event) => {
+  const alwaysOnTop = !mainWindow.isAlwaysOnTop();
+  mainWindow.setAlwaysOnTop(alwaysOnTop);
+});
